fa023a5823d7e9095b180a2a3faa9bca79edba6c
jchamorrobernal <jchamorrobernal@gmail.com>
2020-06-08 19:59:57 -0400

    first commit

---
+++ test/cipher.spec.js
@@ -0,0 +1,39 @@
+describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+      assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 33), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
+    it('deberia retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+      assert.equal(cipher.encode("abcdefghijklmnopqrstuvwxyz", 33), "hijklmnopqrstuvwxyzabcdefg");
+    });
+    it('deberia retornar "3456789012" para "0123456789" con offset 3', () => {
+      assert.equal(cipher.encode("0123456789", 3), "3456789012");
+    });
+  });
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+      assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+    it('deberia retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+      assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33), "abcdefghijklmnopqrstuvwxyz");
+    });
+    it('deberia retornar "0123456789" para "3456789012" con offset 3', () => {
+      assert.equal(cipher.decode("3456789012", 3), "0123456789");
+    });
+  });
+}); 
