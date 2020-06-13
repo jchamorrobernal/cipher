bdc3ac6817f3ce8cba0e14168cff8f763f76f06c
jchamorrobernal <jchamorrobernal@gmail.com>
2020-06-12 20:37:08 -0400

    cambios botones html, cambios en css para botones y flujo lógico en javascript para función principal

---
+++ src/cipher.js
@@ -0,0 +1,32 @@
+// text area 
+
+let inputCifrado1 = document.getElementById("inputingresa1")
+let inputCifrado2 = document.getElementById("inputingresa2")
+let inputDecifrado = document.getElementById("decooutput1")
+let inputDecifrado = document.getElementById("decooutput2")
+
+// buttons
+
+let btnEcpt = document.getElementById("buttonencriptado")
+let btnTipoEcpt = document.getElementById("tipoencriptado")
+let btnDcf = document.getElementById("buttondecifrar") 
+let btnTipoDcf = document.getElementById("tipodecifrado")
+
+function encriptado() {
+    // Flujo logico
+// Lo primero es tomar el valor del texto que ingresa el usuario
+// encriptar el valor tomado
+    //Obtener el código ascii del valor ingresado. -- .charCodeAt()
+        // (fórmula youtube)
+    //sumar clave de cifrado (3) al valor ascii
+    //Convertir el resultado de la suma (ascii) a letra -- String.fromCharCode()
+// Mostrar el valor ya encriptado en caja de texto. 
+}
+
+// flujos lógicos papel y lapiz //
+// recursos, equivocarse y la guía de coach //
+
+function decifrado() {
+
+}
+console.log(String.fromCharCode(72, 111, 108, 97))
